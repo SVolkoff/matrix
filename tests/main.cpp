01d69f7228a0dc3c2179cd03c92eb4fe206d9e29
SVolkoff <sabina-volkoff@yandex.com>
2017-03-12 20:51:50 +0300

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+Matrix m1, m2, m3;
+std ifstream(f1.txt)
+  TEST_CASE( "Factorials are computed", "[factorial]" ) {
+
+   std::ifstream("f1.txt") >> m1;
+   std::ifstream("f2.txt") >> m2;
+   std::ifstream("f3.txt") >> m3;
+   REQUIRE (A+B==C);
+  
+  
+   REQUIRE( Factorial(0) == 1 );
+  
+  
+   REQUIRE( Factorial(1) == 1 );
+   REQUIRE( Factorial(2) == 2 );
+   REQUIRE( Factorial(3) == 6 );
+   REQUIRE( Factorial(10) == 3628800 );
+}
