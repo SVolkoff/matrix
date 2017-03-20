6b31b9242a1ad61b43a604bcb4e5d99edf1193b2
SVolkoff <sabina-volkoff@yandex.com>
2017-03-20 23:09:11 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -13,8 +13,8 @@ public:
 	Matrix();
 	Matrix(int m, int n);
 	Matrix(Matrix &Cpymatr);
-	int lines_()
-	int columns_()
+	int lines_();
+	int columns_();
 	~Matrix();
 	void scan(string fname);
 	friend istream& operator >> (istream& stream, const Matrix& matrix);
